4b40a637172a81a0d82546e8e0563de27be7737a
Moodang2919 <6531501073@lamduan.mfu.ac.th>
2025-04-20 15:00:51 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predictions, preprocess_input
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import pickle
+
+
+with open('model.pkl','rb') as f:
+    model = pickle.load(f)
+    
+st.title("Image Classification with MobileNetV2 by Boonnisa Tangjitpermkwamdee")
+
+upload_file = st.fire_uploader("Upload image",type=["jpg","jpeg","png"])
+
+if upload_file is not None:
+    #display a image on screen
+    img = Image.open(upload_file)
+    st.image(img, caption="upload Image")
+    
+    #preprocessing
+    img = img.resize((224,224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    
+    # Prediction
+    preds = model.predict(x)
+    top_preds = decode_predictions(preds, top=3)[0]
+   
+    st.subheader("Prediction:")
+    for i, pred in enumerate(top_preds):
+       st.write(f"{i+1}. **{pred[1]}** — {round(pred[2]*100, 2)}%")
